253708a5e1aa1cc06381376d07596d40ed067405
MinKyeongTae <kyeongtea92@gmail.com>
2019-12-18 05:19:42 +0900

    H-Index Lv2 Swift 문제풀이

---
+++ PGProblemSolving/Sorting/H-Index_Lv2.swift
@@ -0,0 +1,34 @@
+//
+//  H-Index_Lv2.swift
+//  PGProblemSolving
+//
+//  Created by MinKyeongTae on 2019/12/18.
+//  Copyright © 2019 MinKyeongTae. All rights reserved.
+//
+
+// MARK: H-Index Lv2
+
+/*
+import Foundation
+
+func solution(_ citations:[Int]) -> Int {
+    let citat = citations.sorted()
+    let citatCnt = citations.count
+    for H in stride(from: 1000, to: 0, by: -1) {
+        var qCnt = 0
+        var flag = true
+        for j in citat.indices {
+            if H <= citat[j] { qCnt += 1 }
+            else {
+                if citat[j] > H {
+                    flag = false
+                    break
+                }
+            }
+        }
+        if H <= qCnt && flag { return H }
+    }
+    
+    return 0
+}
+*/

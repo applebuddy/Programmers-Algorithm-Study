d59aee30199ae7c9055f4a0677a2662de39dc0f4
MinKyeongTae <kyeongtea92@gmail.com>
2020-01-25 21:36:29 +0900

    타겟넘버 Lv2 DP C++ 문제풀이

--- PGProblemSolving/DP/타겟넘버DP_43165_Lv2.cpp
+++ PGProblemSolving/DP/타겟넘버DP_43165_Lv2.cpp
@@ -6,7 +6,36 @@
 //  Copyright © 2019 MinKyeongTae. All rights reserved.
 //
 
-/// MARK: - 타겟넘버 : Dynamic Programming version.
+/// MARK: 타겟넘버 : Dynamic Programming version.
+
+// MARK: - 타겟넘버 DP활용 복습 풀이답안
+#if 0
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int answer = 0;
+vector<vector<int>> Cache(21, vector<int>(2001, -1000));
+
+int checkNumber(int idx, int sum, const vector<int> &numbers, const int &target) {
+    if(Cache[idx][sum + 1000] >= 0) return Cache[idx][sum + 1000];
+    if(idx >= numbers.size()) {
+        if(sum == target) {
+            return Cache[idx][sum + 1000] = 1;
+        } else {
+            return Cache[idx][sum + 1000] = 0;
+        }
+    }
+    
+    return Cache[idx][sum+1000] = checkNumber(idx+1, sum+numbers[idx], numbers, target) + checkNumber(idx+1, sum-numbers[idx], numbers, target);
+}
+
+int solution(vector<int> numbers, int target) {
+    return checkNumber(0, 0, numbers, target);
+}
+#endif
+
 #if 0
 #include <string>
 #include <vector>

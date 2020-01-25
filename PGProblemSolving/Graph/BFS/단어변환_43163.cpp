e2b1743d6d569042d9ccbbe8513fe6ac4e841f48
MinKyeongTae <kyeongtea92@gmail.com>
2020-01-26 02:26:13 +0900

    단어변환 Lv3 DFS C++ 문제풀이

--- PGProblemSolving/Graph/BFS/단어변환_43163.cpp
+++ PGProblemSolving/Graph/BFS/단어변환_43163.cpp
@@ -6,7 +6,50 @@
 //  Copyright © 2019 MinKyeongTae. All rights reserved.
 //
 
-/// MARK: - 단어변환
+/// MARK: 단어변환 Lv3
+
+// MARK: - 단어변환 DFS사용 문제풀이
+#if 0
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int answer = 0;
+map<string,int> MP;
+
+bool checkOneDiff(string now, string next) {
+    int diff = 0;
+    for(int i=0; i<now.length(); i++) {
+        if(now[i] != next[i]) diff++;
+        if(diff > 1) return false;
+    }
+    return diff == 1;
+}
+
+void DFS(string now, int count, string target, vector<string> words) {
+    if(now == target) {
+        answer = count;
+        return;
+    }
+    
+    for(int i=0; i<words.size(); i++) {
+        if(MP[words[i]]!=0 || checkOneDiff(now, words[i])==false) continue;
+        MP[words[i]]=1;
+        DFS(words[i], count+1, target, words);
+        MP[words[i]]=0;
+    }
+    
+}
+
+int solution(string begin, string target, vector<string> words) {
+    DFS(begin, 0, target, words);
+    return answer;
+}
+#endif
+
+// MARK: - Set, BFS 사용 문제풀이 (DFS보다 빠름)
 #if 0
 #include <string>
 #include <vector>

4dc1b647b9fa74a748a2023ce0e48b27ef0a7354
MinKyeongTae <kyeongtea92@gmail.com>
2020-01-27 01:29:36 +0900

    숫자게임 Lv3 Greedy C++ 문제풀이

--- PGProblemSolving/Greedy/숫자게임_12987_Lv3.cpp
+++ PGProblemSolving/Greedy/숫자게임_12987_Lv3.cpp
@@ -6,7 +6,33 @@
 //  Copyright © 2019 MinKyeongTae. All rights reserved.
 //
 
-/// MARK: - 숫자게임_12987_Lv3
+/// MARK: 숫자게임 Lv3 12987
+
+// MARK: - 숫자게임 복습 풀이답안
+#if 0
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B) {
+    int answer = 0;
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    int idxA = 0, idxB = 0;
+    while(idxA < A.size() && idxB < B.size()) {
+        if(A[idxA] < B[idxB]) {
+            answer++;
+            idxA++;
+        }
+        idxB++;
+    }
+    return answer;
+}
+#endif
+
+// MARK: - 이전 풀이답안
 #if 0
 #include <string>
 #include <vector>

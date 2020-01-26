7a3482b31413433f9793e417572fbff8d49eeb46
MinKyeongTae <kyeongtea92@gmail.com>
2020-01-27 01:17:57 +0900

    숫자게임 Lv3 Greedy Swift 문제풀이

---
+++ PGProblemSolving/Greedy/숫자게임_Lv3.swift
@@ -0,0 +1,27 @@
+//
+//  숫자게임_Lv3.swift
+//  PGProblemSolving
+//
+//  Created by MinKyeongTae on 2020/01/27.
+//  Copyright © 2020 MinKyeongTae. All rights reserved.
+//
+
+// MARK: 숫자게임 문제풀이
+/*
+import Foundation
+
+func solution(_ a:[Int], _ b:[Int]) -> Int {
+    let arrA = a.sorted()
+    let arrB = b.sorted()
+    var idxA = 0, idxB = 0, Ans = 0
+    while idxA < arrA.count && idxB < arrB.count {
+        if arrA[idxA] < arrB[idxB] {
+            Ans += 1
+            idxA += 1
+        }
+        idxB += 1
+    }
+    
+    return Ans
+}
+*/

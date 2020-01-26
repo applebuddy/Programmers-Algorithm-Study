043e0dcf6da4d32d2e131a988e594bab9ab73d2c
MinKyeongTae <kyeongtea92@gmail.com>
2020-01-26 19:10:05 +0900

    네트워크 Lv3 DFS Swift 문제풀이

--- PGProblemSolving/Graph/DFS/네트워크_Lv3.swift
+++ PGProblemSolving/Graph/DFS/네트워크_Lv3.swift
@@ -6,8 +6,39 @@
 //  Copyright © 2019 MinKyeongTae. All rights reserved.
 //
 
-// MARK: - 네크워크_43162_Lv3
+// MARK: 네크워크_43162_Lv3
 
+// MARK: - 입력배열 활용 풀이답안
+/*
+import Foundation
+
+var answer = 0
+var C = [Int](repeating: 0, count: 201)
+
+func DFS(_ node: Int, _ computers: inout [[Int]]) {
+    C[node] = 1
+    for i in computers[node].indices {
+        if C[i] == 1 || computers[node][i] == 0 { continue }
+        C[i] = 1
+        DFS(i, &computers)
+    }
+}
+
+func solution(_ n:Int, _ computers:[[Int]]) -> Int {
+    var Comp = computers
+    for i in 0..<n {
+        if C[i]==0 {
+            C[i]=1
+            answer += 1
+            DFS(i, &Comp)
+        }
+    }
+    return answer
+}
+ */
+
+// MARK: - 예전 풀이답안(인접리스트 재구성 후 풀이답안)
+/*
 import Foundation
 
 var G = [[Int]](repeating: [Int](), count: 201)
@@ -42,3 +73,4 @@ func solution(_: Int, _ computers: [[Int]]) -> Int {
 
     return Ans
 }
+*/

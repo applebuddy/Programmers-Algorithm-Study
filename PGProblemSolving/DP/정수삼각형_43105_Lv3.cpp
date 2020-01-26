1a1917e272d7ff3438c48357ad528715ecd7bbdc
MinKyeongTae <kyeongtea92@gmail.com>
2020-01-26 20:14:16 +0900

    정수삼각형 Lv3 DP C++ 문제풀이

--- PGProblemSolving/DP/정수삼각형_43105_Lv3.cpp
+++ PGProblemSolving/DP/정수삼각형_43105_Lv3.cpp
@@ -6,7 +6,30 @@
 //  Copyright © 2019 Min Kyeong Tae. All rights reserved.
 //
 
-/// MARK: - 정수삼각형_43105
+/// MARK: 정수삼각형 Lv3 43105
+
+// MARK: - 정수삼각형 복습 풀이답안
+#if 0
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> triangle) {
+    int answer = triangle[0][0];
+    for(int i=1; i<triangle.size(); i++) {
+        for(int j=0; j<triangle[i].size(); j++) {
+            if(j==0) triangle[i][j] += triangle[i-1][j];
+            else if(j==triangle[i].size()-1) triangle[i][j] += triangle[i-1][triangle[i-1].size()-1];
+            else triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j]);
+            answer = triangle[i][j] > answer ? triangle[i][j] : answer;
+        }
+    }
+    return answer;
+}
+#endif
+
+// MARK: - 정수삼각형 풀이답안
 #if 0
 #include <iostream>
 #include <string>

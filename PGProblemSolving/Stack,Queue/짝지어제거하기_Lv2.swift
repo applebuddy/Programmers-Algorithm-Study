43bf20a2138c1d74048b8f3feaa1f84e70272746
MinKyeongTae <kyeongtea92@gmail.com>
2020-01-27 18:55:01 +0900

    짝지어제거하기 LV2 Swift 문제풀이

--- PGProblemSolving/Stack,Queue/짝지어제거하기_Lv2.swift
+++ PGProblemSolving/Stack,Queue/짝지어제거하기_Lv2.swift
@@ -6,10 +6,32 @@
 //  Copyright © 2019 Min Kyeong Tae. All rights reserved.
 //
 
-// MARK: - 짝지어 제거하기 : Word Compression
+// MARK: 짝지어 제거하기 Lv2 Word Compression
 
-// MARK: 연속으로 같은 문자열을 하나씩 제거해서 모두 제거할 수 있는지 여부를 확인해라!!!
+// MARK: - 연속으로 같은 문자열을 하나씩 제거해서 모두 제거할 수 있는지 여부를 확인해라!!!
 
+// MARK: - 짝지어 제거하기 복습 문제풀이
+/*
+import Foundation
+
+func solution(_ s:String) -> Int{
+    var STK = [Character]()
+    let arr = Array(s)
+    
+    for i in arr.indices {
+        if STK.isEmpty {
+            STK.append(arr[i])
+        } else {
+            if STK.last! == arr[i] { // 같은게 있을때 제거만 해도 정답처리가 된다.
+                STK.removeLast()
+            }
+        }
+    }
+    return STK.isEmpty ? 1 : 0
+}
+*/
+
+// MARK: - 짝지어 제거하기 이전 문제풀이
 /*
  import Foundation
 

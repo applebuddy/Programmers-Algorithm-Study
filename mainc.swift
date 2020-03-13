5f16a18f20f41036eef8ebb27dac3679087b05ee
MinKyeongTae <kyeongtea92@gmail.com>
2020-03-13 21:44:18 +0900

    PS 폴더정리

--- mainc.swift
+++ mainc.swift
@@ -7,23 +7,23 @@
 //
 
 /*
-// MARK: 수 알아내기_1920
+ // MARK: 수 알아내기_1920
 
-import Foundation
+ import Foundation
 
-let N = Int(readLine()!)!
-var dic = [Int:Int]()
+ let N = Int(readLine()!)!
+ var dic = [Int:Int]()
 
-let nArr = readLine()!.split(separator: " ").map { Int($0)! }
-for i in nArr.indices {
-    dic[nArr[i]] = (dic[nArr[i]] ?? 0) + 1
-}
+ let nArr = readLine()!.split(separator: " ").map { Int($0)! }
+ for i in nArr.indices {
+ dic[nArr[i]] = (dic[nArr[i]] ?? 0) + 1
+ }
 
-let M = Int(readLine()!)!
+ let M = Int(readLine()!)!
 
-let mArr = readLine()!.split(separator: " ").map { Int($0)! }
-for i in mArr.indices {
-    let chk = dic[mArr[i]] == nil ? 0 : 1
-    print(chk)
-}
-*/
+ let mArr = readLine()!.split(separator: " ").map { Int($0)! }
+ for i in mArr.indices {
+ let chk = dic[mArr[i]] == nil ? 0 : 1
+ print(chk)
+ }
+ */

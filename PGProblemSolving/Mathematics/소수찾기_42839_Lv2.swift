292110996050566b6bc20d9c25c021ae7de8029d
MinKyeongTae <kyeongtea92@gmail.com>
2020-07-15 20:03:53 +0900

    소수찾기_42839_Lv2 swift 복습 문제풀이(최적화)

--- PGProblemSolving/Mathematics/소수찾기_42839_Lv2.swift
+++ PGProblemSolving/Mathematics/소수찾기_42839_Lv2.swift
@@ -8,6 +8,57 @@
 
 // MARK: - 소수찾기 Lv2
 
+// MARK: 단순배열 사용, n배 빠른버전
+
+import Foundation
+
+func solution(_ numbers: String) -> Int {
+    let arr = Array(numbers).map { Int(String($0))! }
+    let max = Int(arr.sorted(by: >).map { String($0) }.joined())!
+    var dic = [Int](repeating: 0, count: 10)
+    var Ans = 0
+    var P = [Bool](repeating: true, count: max+1)
+    var idx = 2, jdx = idx + idx
+    while idx * idx <= max {
+        if P[idx] {
+            jdx = idx + idx
+            while jdx <= max {
+                P[jdx] = false
+                jdx += idx
+            }
+        }
+        idx += 1
+    }
+    
+    for i in arr.indices {
+        dic[arr[i]] += 1
+    }
+    
+    for i in 2...max {
+        if P[i] {
+            var num = i
+            var tDic = dic
+            var isValid = true
+            while num > 0 {
+                if tDic[num % 10] <= 0 {
+                    isValid = false
+                    break
+                }
+                tDic[num % 10] -= 1
+                num /= 10
+            }
+            
+            if isValid { Ans += 1 }
+        }
+    }
+    
+    return Ans
+}
+
+
+// MARK: 딕셔너리 사용, 느린버전
+
+/*
 import Foundation
 
 var checkDic = [Int: Int]()
@@ -51,3 +102,4 @@ func findThePrime(_ numbers: String) -> Int {
 
     return Ans
 }
+*/

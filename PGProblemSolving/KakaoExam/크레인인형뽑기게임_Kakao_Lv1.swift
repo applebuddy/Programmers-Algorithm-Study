708627d96c85e912700199559f5a24549b3f9290
MinKyeongTae <kyeongtea92@gmail.com>
2020-07-26 23:42:02 +0900

    오픈채팅방_42888_Lv3 Hash swift 문제풀이

--- PGProblemSolving/KakaoExam/크레인인형뽑기게임_Kakao_Lv1.swift
+++ PGProblemSolving/KakaoExam/크레인인형뽑기게임_Kakao_Lv1.swift
@@ -1,32 +1,33 @@
 
 // MARK: - 크레인인형뽑기게임 Kakao Lv1
+
 // MARK: Stack swift 문제풀이
 
 /*
-import Foundation
+ import Foundation
 
-func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
-    let len = board.count
-    var SV = [[Int]](repeating: [Int](), count: len)
-    var Ans = 0
-    var basket = [Int]()
-    for i in board.indices.reversed() {
-        for j in board[i].indices {
-            if board[i][j] != 0 { SV[j].append(board[i][j]) }
-        }
-    }
-    
-    for i in moves.indices {
-        let idx = moves[i] - 1
-        if !SV[idx].isEmpty {
-            let doll = SV[idx].last!
-            SV[idx].removeLast()
-            if !basket.isEmpty {
-                if doll == basket.last! { Ans += 2; basket.removeLast() }
-                else { basket.append(doll) }
-            } else { basket.append(doll) }
-        }
-    }
-    return Ans
-}
-*/
+ func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
+ let len = board.count
+ var SV = [[Int]](repeating: [Int](), count: len)
+ var Ans = 0
+ var basket = [Int]()
+ for i in board.indices.reversed() {
+     for j in board[i].indices {
+         if board[i][j] != 0 { SV[j].append(board[i][j]) }
+     }
+ }
+
+ for i in moves.indices {
+     let idx = moves[i] - 1
+     if !SV[idx].isEmpty {
+         let doll = SV[idx].last!
+         SV[idx].removeLast()
+         if !basket.isEmpty {
+             if doll == basket.last! { Ans += 2; basket.removeLast() }
+             else { basket.append(doll) }
+         } else { basket.append(doll) }
+     }
+ }
+ return Ans
+ }
+ */
--- PGProblemSolving/KakaoExam/튜플_64065_Kakao_Lv2.swift
+++ PGProblemSolving/KakaoExam/튜플_64065_Kakao_Lv2.swift
@@ -1,54 +1,55 @@
 
 // MARK: - 튜플 Kakao Lv2
+
 // MARK: Set swift 문제풀이
 
 /*
-import Foundation
+ import Foundation
 
-func solution(_ s:String) -> [Int] {
-    var stk = 0
-    let arr = Array(s).map { String($0) }
-    var tmpST = Set<String>()
-    var tmpStr = ""
-    var Ans = [Int]()
-    var STV = Array<Set<String>>()
-    for i in arr.indices {
-        if arr[i] == "{" {
-            stk += 1
-            continue
-        } else if arr[i] == "}" {
-            stk -= 1
-            if !tmpST.isEmpty || !tmpStr.isEmpty {
-                tmpST.insert(tmpStr)
-                tmpStr = ""
-                STV.append(tmpST)
-                tmpST = Set<String>()
-            }
-            continue
-        }
+ func solution(_ s:String) -> [Int] {
+ var stk = 0
+ let arr = Array(s).map { String($0) }
+ var tmpST = Set<String>()
+ var tmpStr = ""
+ var Ans = [Int]()
+ var STV = Array<Set<String>>()
+ for i in arr.indices {
+     if arr[i] == "{" {
+         stk += 1
+         continue
+     } else if arr[i] == "}" {
+         stk -= 1
+         if !tmpST.isEmpty || !tmpStr.isEmpty {
+             tmpST.insert(tmpStr)
+             tmpStr = ""
+             STV.append(tmpST)
+             tmpST = Set<String>()
+         }
+         continue
+     }
 
-        if stk == 2 {
-            if arr[i] == "," {
-                tmpST.insert(tmpStr)
-                tmpStr = ""
-            } else {
-                tmpStr += arr[i]
-            }
-        }
-    }
+     if stk == 2 {
+         if arr[i] == "," {
+             tmpST.insert(tmpStr)
+             tmpStr = ""
+         } else {
+             tmpStr += arr[i]
+         }
+     }
+ }
 
-    STV.sort { $0.count < $1.count }
-    for st in STV.first! {
-        Ans.append(Int(st)!)
-    }
+ STV.sort { $0.count < $1.count }
+ for st in STV.first! {
+     Ans.append(Int(st)!)
+ }
 
-    let len = STV.count
-    if len > 1 {
-        for i in 0..<len-1 {
-            Ans.append(Int(STV[i].symmetricDifference(STV[i+1]).joined())!)
-        }
-    }
+ let len = STV.count
+ if len > 1 {
+     for i in 0..<len-1 {
+         Ans.append(Int(STV[i].symmetricDifference(STV[i+1]).joined())!)
+     }
+ }
 
-    return Ans
-}
-*/
+ return Ans
+ }
+ */

f843868f4d476c2ee0fe20e5b0f3e35cfe261a50
MinKyeongTae <kyeongtea92@gmail.com>
2020-09-07 01:17:41 +0900

    키패드누르기_67256 Implementation swift 풀이 코드정리

--- PGProblemSolving/KakaoExam/키패드누르기_67256.swift
+++ PGProblemSolving/KakaoExam/키패드누르기_67256.swift
@@ -11,49 +11,39 @@ func isRightPos(_ num: Int) -> Bool {
     return num == 3 || num == 6 || num == 9
 }
 
-func solution(_ numbers: [Int], _ hand: String) -> String {
+func solution(_ numbers:[Int], _ hand:String) -> String {
     var L = 10, R = 11
-    let pos = [(3, 1), (0, 0), (0, 1), (0, 2),
-               (1, 0), (1, 1), (1, 2),
-               (2, 0), (2, 1), (2, 2), (3, 0), (3, 2)]
+    let pos = [(3,1), (0,0), (0,1), (0,2),
+               (1,0), (1,1), (1,2),
+               (2,0), (2,1), (2,2), (3,0), (3,2)]
     var Ans = ""
-
+    
     numbers.forEach {
-        let leftDist = Int(abs(Double(pos[$0].0 - pos[L].0))
-            + abs(Double(pos[$0].1 - pos[L].1)))
-        let rightDist = Int(abs(Double(pos[$0].0 - pos[R].0))
-            + abs(Double(pos[$0].1 - pos[R].1)))
-        if hand == "left" {
-            if isLeftPos($0) {
+        let leftDist = Int(abs(Double((pos[$0].0 - pos[L].0)))
+            + abs(Double((pos[$0].1 - pos[L].1))))
+        let rightDist = Int(abs(Double((pos[$0].0 - pos[R].0)))
+            + abs(Double((pos[$0].1 - pos[R].1))))
+        if isLeftPos($0) {
+            L = $0
+            Ans += "L"
+        } else if isRightPos($0) {
+            R = $0
+            Ans += "R"
+        } else if hand == "left" {
+            if leftDist <= rightDist {
                 L = $0
                 Ans += "L"
-            } else if isRightPos($0) {
+            } else {
                 R = $0
                 Ans += "R"
-            } else {
-                if leftDist <= rightDist {
-                    L = $0
-                    Ans += "L"
-                } else {
-                    R = $0
-                    Ans += "R"
-                }
             }
         } else {
-            if isRightPos($0) {
+            if leftDist >= rightDist {
                 R = $0
                 Ans += "R"
-            } else if isLeftPos($0) {
+            } else {
                 L = $0
                 Ans += "L"
-            } else {
-                if leftDist >= rightDist {
-                    R = $0
-                    Ans += "R"
-                } else {
-                    L = $0
-                    Ans += "L"
-                }
             }
         }
     }
